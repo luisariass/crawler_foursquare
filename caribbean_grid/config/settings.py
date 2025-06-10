893faa2a357a6c8402547b945d671fdd65b6506d
luisariass <ariasl@utb.edu.co>
2025-06-09 23:42:09 -0500

    ADD: file settings v1 ADD: list key departments caribbean, API url and directory output

---
+++ caribbean_grid/config/settings.py
@@ -0,0 +1,8 @@
+CARIBBEAN_DEPARTMENTS = [
+    "BOLÍVAR", "ATLÁNTICO", "LA GUAJIRA", "SUCRE",
+    "ARCHIPIÉLAGO DE SAN ANDRÉS, PROVIDENCIA Y SANTA CATALINA",
+    "CÓRDOBA", "CESAR", "MAGDALENA"
+]
+
+API_URL = "https://www.datos.gov.co/resource/gdxc-w37w.json"
+DEFAULT_OUTPUT_DIR = "municipios_csv"
