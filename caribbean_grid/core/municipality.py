d1a3a12d7b25ca26b2119d7c7334899d571f619d
luisariass <ariasl@utb.edu.co>
2025-06-16 20:03:46 -0500

    UPDATE: change name api url import settings, API_URL to DATOSGOV_API UPDATE: changed method name, fetch_official_municipalities to fetch_municipalities

--- caribbean_grid/core/municipality.py
+++ caribbean_grid/core/municipality.py
@@ -1,10 +1,10 @@
 import requests
-from config.settings import API_URL
+from config.settings import DATOSGOV_API
 
-def fetch_official_municipalities(department: str):
+def fetch_municipalities(department: str):
     params = {
         "$query": f"SELECT cod_dpto, dpto, cod_mpio, nom_mpio, tipo_municipio, longitud, latitud WHERE dpto = '{department}'"
     }
-    response = requests.get(API_URL, params=params)
+    response = requests.get(DATOSGOV_API, params=params)
     response.raise_for_status()
     return response.json()
