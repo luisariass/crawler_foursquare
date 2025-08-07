a55096604d977642c32a15d12e0720fb2faf2f40
luisariass <ariasl@utb.edu.co>
2025-08-06 21:58:10 -0500

    UPDATE: refactor generate_zones function to improve shapefile loading and polygon processing

--- caribbean_grid/h3_fetcher.py
+++ caribbean_grid/h3_fetcher.py
@@ -1,33 +1,24 @@
 import pandas as pd
 import glob
 import os
-import unicodedata
-import geopandas as gpd
 from config.settings import ZONES_OUTPUT_DIR
-from utils.shapefile_helpers import load_municipal_boundaries, find_cabecera_polygon
+from utils.shapefile_helpers import load_boundaries, find_polygon
 from utils.h3_helpers import get_h3_cells_from_polygon, h3_cell_to_center, h3_cell_to_bbox
 
-def normalize_text(text):
-    if not isinstance(text, str):
-        return ""
-    text = text.lower()
-    text = ''.join(c for c in unicodedata.normalize('NFD', text) if unicodedata.category(c) != 'Mn')
-    return text.strip()
-
-def generate_zones_from_shapefile():
+def generate_zones():
     SHAPEFILE_PATH = "caribbean_grid/data/MGN2020_URB_AREA_CENSAL"
     
     try:
-        gdf = load_municipal_boundaries(SHAPEFILE_PATH)
+        gdf = load_boundaries(SHAPEFILE_PATH)
         gdf = gdf.to_crs("EPSG:4326")
     except Exception as e:
         print(f"Error fatal: No se pudo cargar o procesar el Shapefile. {e}")
         return
 
     csv_files = glob.glob("caribbean_grid/data/municipios_*.csv")
+
     for csv_file in csv_files:
         departamento = os.path.basename(csv_file).replace("municipios_", "").replace(".csv", "").replace("_", " ").title()
-        #df = pd.read_csv(csv_file)
         df = pd.read_csv(csv_file, dtype={'cod_dpto': str, 'cod_mpio': str})
         rows = []
         print(f"\n--- Procesando Departamento: {departamento} ---")
@@ -38,21 +29,19 @@ def generate_zones_from_shapefile():
             cod_mpio = str(row.get("cod_mpio", "")).strip()
             
             print(f"Procesando {municipio} (Código: {cod_mpio})...")
-            
-            polygon = find_cabecera_polygon(gdf, cod_dpto, cod_mpio)
-            
+
+            polygon = find_polygon(gdf, cod_dpto, cod_mpio)
+
             if polygon and not polygon.is_empty:
                 resolution = 10
-                
                 if polygon.area < 0.001:
                     resolution = 11
-                
+
                 h3_cells = get_h3_cells_from_polygon(polygon, resolution=resolution)
                 
                 if len(h3_cells) > 250 and resolution > 8:
                     resolution -= 1
                     h3_cells = get_h3_cells_from_polygon(polygon, resolution=resolution)
-                
                 if len(h3_cells) < 3 and resolution < 11:
                     resolution += 1
                     h3_cells = get_h3_cells_from_polygon(polygon, resolution=resolution)
@@ -81,4 +70,4 @@ def generate_zones_from_shapefile():
             print(f"Archivo generado: {out_path}")
 
 if __name__ == "__main__":
-    generate_zones_from_shapefile()
+    generate_zones()
