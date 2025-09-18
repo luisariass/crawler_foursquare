b948db571afdae9eb2ad83ec3df3970529ae2091
luisariass <ariasl@utb.edu.co>
2025-09-17 21:06:41 -0500

    UPDATE: adjust user agent and viewport settings. UPDATE: modify parallel process count for improved performance

--- model_sities/config/settings.py
+++ model_sities/config/settings.py
@@ -29,7 +29,20 @@ class Settings:
     FAILED_MUNICIPALITIES_PATH = os.path.join(DATA_DIR, "failed_municipalities.txt") # Archivo para registrar municipios fallidos
     STOP_FILE_PATH = os.path.join(DATA_DIR, "stop_scraping.flag") # Archivo para controlar la pausa
 
-
+    USER_AGENTS = [
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/113.0',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'
+    ]
+    VIEWPORTS = [
+        {'width': 1920, 'height': 1080},
+        {'width': 1366, 'height': 768},
+        {'width': 1536, 'height': 864}
+    ]
+    
     # URLs de Foursquare
     BASE_URL = "https://es.foursquare.com"
     LOGIN_URL = f"{BASE_URL}/login"
@@ -65,7 +78,7 @@ class Settings:
     SAVE_INTERVAL = 5  # Guardar cada 5 URLs procesadas
     RETRIES = 3  # Número de reintentos al fallar una URL
     TIMEOUT = 60000
-    PARALLEL_PROCESSES = 4 # Número de procesos a ejecutar en paralelo
+    PARALLEL_PROCESSES = 1 # Número de procesos a ejecutar en paralelo
     BACKOFF_FACTOR = 10 # Factor para el backoff progresivo en reintentos
 
     # Selectores CSS
